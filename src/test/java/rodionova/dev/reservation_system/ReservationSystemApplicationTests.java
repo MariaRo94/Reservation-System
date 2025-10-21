6eef67e397f937be3f6ee8c79504e1d04e707c1a
Maria Rodionova <mrodionova@aflt-systems.ru>
2025-10-21 19:03:19 +0300

    First commit

---
+++ src/test/java/rodionova/dev/reservation_system/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package rodionova.dev.reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
